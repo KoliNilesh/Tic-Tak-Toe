f7d6c993b8084915b0b62234ad703eb3714a13ea
Nilesh Bhagwat Koli <153268077+KoliNilesh@users.noreply.github.com>
2024-02-22 21:09:04 +0530

    Tic-Tak-Toe - Javascript

---
+++ App.js
@@ -0,0 +1,78 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO =true // playerX,playerO
+
+let winPatterns =[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+boxes.forEach((box)=>{
+    box.addEventListener('click',()=>{
+        // console.log("Box was clicked..");
+        if(turnO){
+            box.innerText = "❌";
+            turnO = false;
+
+        }else{
+            box.innerText = "⭕";
+            turnO = true;
+        }
+        box.disabled=true;
+        checkWinner();
+    })
+});
+
+const disableBoxes =()=>{
+    for(let box of boxes)
+    {
+        box.disabled=true;
+    }
+};
+const showWinner = (winer)=>{
+    msg.innerText =`✨🎇🏆 Congratulation  Winner is ${winer} 🏆🎇✨`;
+    
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+const checkWinner=()=>{
+    for(pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val && pos3Val){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                showWinner(pos1Val);
+            }
+        }
+
+    }
+
+};
+
+const resetGame=()=>{
+    turnO=true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+
+const enableBoxes =()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+};
+newGameBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
